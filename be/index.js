343294bce7fb7413bc0af9e59464ecd198992a99
FaurIonut <162836427+FaurIonut@users.noreply.github.com>
2024-07-23 13:38:25 +0300

    update index js

--- be/index.js
+++ be/index.js
@@ -1,21 +1,21 @@
-const express=require('express')
-const app=express()
-const port=5000
-const cors=require('cors')
-const connect=require('./db')
-connect()
-require('dotenv').config()
-app.use(cors())
-app.use(express.json())
-app.use("/validate",require('./Routes/validate'))
-app.use("/boosters",require('./Routes/Boosters'))
-app.use("/referral",require('./Routes/Referral'))
-app.use("/update",require('./Routes/Update'))
-app.use("/tasks",require('./Routes/Tasks'))
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connect = require('./db');
 
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
+connect();
 
+app.use(cors());
+app.use(express.json());
+app.use("/validate", require('./Routes/validate'));
+app.use("/boosters", require('./Routes/Boosters'));
+app.use("/referral", require('./Routes/Referral'));
+app.use("/update", require('./Routes/Update'));
+app.use("/tasks", require('./Routes/Tasks'));
 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
